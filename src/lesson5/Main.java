cc33c08ed564f35f92ec23197add8a045b160c48
Pavel A. Khanin <pkhanyn@gmail.com>
2021-02-16 13:15:35 +0300

    add method 3

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,25 +1,26 @@
 package lesson5;
 
+import java.util.ArrayList;
+
 public class Main {
 
     static final int size = 10000000;
 
+    static float[] arr = new float[size];
+
     public static void main(String[] args) {
         method1();
         method2();
+        method3(3);
     }
 
-    private static void method1() {
-        float[] arr = new float[size];
+    private static void fillArray() {
         for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
+    }
 
-        long a = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        System.out.println(System.currentTimeMillis() - a);
+    private static void checkSum() {
         float sum = 0;
         for (int i = 0; i < size; i++) {
             sum+=arr[i];
@@ -27,32 +28,37 @@ public class Main {
         System.out.println(sum);
     }
 
-    private static void method2() {
-        float[] arr = new float[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = 1;
-        }
-        int h = size / 2;
-
+    private static void method1() {
+        fillArray();
         long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+        checkSum();
+    }
 
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
+    private static void method2() {
+        fillArray();
+        int h = size / 2;
+        long a = System.currentTimeMillis();
         Thread th1 = new Thread(()->{
-            System.arraycopy(arr, 0, a1, 0, h);
+            float[] ac = new float[h];
+            System.arraycopy(arr, 0, ac, 0, h);
             for (int i = 0; i < h; i++) {
-                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                ac[i] = (float)(ac[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-            System.arraycopy(a1, 0, arr, 0, h);
+            System.arraycopy(ac, 0, arr, 0, h);
         });
         th1.start();
         Thread th2 = new Thread(()->{
-            System.arraycopy(arr, h, a2, 0, h);
+            float[] ac = new float[h];
+            System.arraycopy(arr, h, ac, 0, h);
             for (int i = h; i < size; i++) {
                 int c = i - h;
-                a2[c] = (float)(a2[c] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                ac[c] = (float)(ac[c] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-            System.arraycopy(a2, 0, arr, h, h);
+            System.arraycopy(ac, 0, arr, h, h);
         });
         th2.start();
         try {
@@ -62,12 +68,54 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(System.currentTimeMillis() - a);
-        float sum = 0;
-        for (int i = 0; i < size; i++) {
-            sum+=arr[i];
-        }
-        System.out.println(sum);
+        checkSum();
+    }
 
+    private static void method3(int threadCount) {
+        fillArray();
+        long a = System.currentTimeMillis();
+        int partSize = size / threadCount;
+        ArrayList<Thread> threads = new ArrayList<>();
+        int srcPos = 0;
+        for (int i = 0; i < threadCount; i++) {
+            int finalSrcPos = srcPos;
+            Thread thread = new Thread(()->{
+                float[] ac = new float[partSize];
+                System.arraycopy(arr, finalSrcPos, ac, 0, partSize);
+                for (int j = 0; j < partSize; j++) {
+                    int c = j + finalSrcPos;
+                    ac[j] = (float)(ac[j] * Math.sin(0.2f + c / 5) * Math.cos(0.2f + c / 5) * Math.cos(0.4f + c / 2));
+                }
+                System.arraycopy(ac, 0, arr, finalSrcPos, partSize);
+            });
+            thread.start();
+            threads.add(thread);
+            srcPos+=partSize;
+        }
+        if (srcPos < size) {
+            int finalSrcPos1 = srcPos;
+            Thread thread = new Thread(()->{
+                float[] ac = new float[partSize];
+                int p = size - finalSrcPos1;
+                System.arraycopy(arr, finalSrcPos1, ac, 0, p);
+                for (int j = 0; j < p; j++) {
+                    int c = j + finalSrcPos1;
+                    ac[j] = (float)(ac[j] * Math.sin(0.2f + c / 5) * Math.cos(0.2f + c / 5) * Math.cos(0.4f + c / 2));
+                }
+                System.arraycopy(ac, 0, arr, finalSrcPos1, p);
+            });
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread: threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis() - a);
+        checkSum();
     }
 
 }
